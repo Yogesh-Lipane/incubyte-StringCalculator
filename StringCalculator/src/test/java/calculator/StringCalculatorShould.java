20d0af8cb98fdc5789dfacf65acff7a7fe3cc0df
Yogesh Lipane <yogeshlipane1996@gmail.com>
2021-10-16 15:10:43 +0530

    initial commit for new java project

---
+++ StringCalculator/src/test/java/calculator/StringCalculatorShould.java
@@ -0,0 +1,16 @@
+package test.java.calculator;
+import main.java.calculator.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorShould {
+
+    @Test
+    void empty_string_should_return_0() {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(0, stringCalculator.add(""));
+    }
+
+  
+}
