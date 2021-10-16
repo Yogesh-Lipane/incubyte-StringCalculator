73e915cd7ceeae08de7901b60875103615700104
Yogesh Lipane <yogeshlipane1996@gmail.com>
2021-10-16 15:41:11 +0530

    Allow the Add method to handle new lines between numbers (instead of commas).

--- StringCalculator/src/main/java/calculator/StringCalculator.java
+++ StringCalculator/src/main/java/calculator/StringCalculator.java
@@ -1,10 +1,10 @@
 package main.java.calculator;
 
 public class StringCalculator {
-
+	private final static String delimeter = ",|\n";
     public static int add(String numbers) {
     	 int returnValue = 0;
-    	 String[] numbersArray = numbers.split(",");
+    	 String[] numbersArray = numbers.split(delimeter);
 //         if (numbersArray.length > 2) {
 //             throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
 //         } else {
--- StringCalculator/src/test/java/calculator/StringCalculatorShould.java
+++ StringCalculator/src/test/java/calculator/StringCalculatorShould.java
@@ -24,4 +24,8 @@ class StringCalculatorShould {
 	    public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
 	        Assert.assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
 	 }
+	 @Test
+	    public final void newLineBetweenNumbers() {
+	        Assert.assertEquals(6, StringCalculator.add("1\n2,3"));
+	 }
 }
